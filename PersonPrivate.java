c0aa5493ddbf3739d807cf753356e077d68126ae
CJ1412 <138686340+CJTan0503@users.noreply.github.com>
2024-11-26 17:31:03 +0800

    Assignment 1

--- PersonPrivate.java
+++ PersonPrivate.java
@@ -1,19 +1,31 @@
 public class PersonPrivate {
+    //Private fields
+    
     private String name;
     private int age;
+    private double height;
+    private double weight;
 
-    public PersonPrivate(String name, int age) {
+
+    //Constructor
+    public PersonPrivate(String name, int age, double height, double weight) {
         this.name = name;
         this.age = age;
+        this.height = height;
+        this.weight = weight;
+
     }
 
     public void displayDetails() {
-        System.out.println("Name: " + name);
-        System.out.println("Age: " + age);
+        double bmi = height / (weight + age);
+        System.out.println(name + "bmis is: " + bmi);
+        /*System.out.println("Name: " + name);
+        System.out.println("Age: " + age);*/
+        
     }
 
     public static void main(String[] args) {
-        PersonPrivate person = new PersonPrivate("Tan", 30);
+        PersonPrivate person = new PersonPrivate("Tan", 30, 175.65, 95.4);
         person.displayDetails();
     }
 }
